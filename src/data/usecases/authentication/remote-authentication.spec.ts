37fe354c6399bb46e4f60ffd53b2cfb2174de21a
Fabio <fabioac71@gmail.com>
2021-08-19 11:53:34 -0300

    feat: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,7 +1,10 @@
+import faker from 'faker';
+
 import { RemoteAuthentication } from './remote-authentication';
 import { HttpPostClientSpy } from 'data/test/mock-http-client';
-import faker from 'faker';
 import { mockAuthentication } from 'domain/test/mock-authentication';
+import { InvalidCredentialsError } from 'domain/errors/invalid-credentials-error';
+import { HttpStatusCode } from 'data/protocols/http/http-response';
 
 type SutTypes = {
     sut: RemoteAuthentication,
@@ -19,17 +22,27 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
 
 describe('RemoteAuthentication', () => {
 
-    test('should call HttpPostClient with correct URL', async () => {
+    test('Should call HttpPostClient with correct URL', async () => {
         const url = faker.internet.url();
         const {sut, httpPostClientSpy} = makeSut(url)
         await sut.auth(mockAuthentication())
         expect(httpPostClientSpy.url).toBe(url);
-    })
+    });
 
-    test('should call HttpPostClient with correct body', async () => {
+    test('Should call HttpPostClient with correct body', async () => {
         const {sut, httpPostClientSpy} = makeSut();
         const authenticationParams = mockAuthentication()
         await sut.auth(authenticationParams)
         expect(httpPostClientSpy.body).toEqual(authenticationParams);
+    });
+
+    test('Should throw InvalidCredentialsError if HttpPostClient returns 401' , async () => {
+        const {sut, httpPostClientSpy} = makeSut();
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.unathorized
+        }
+
+        const promise = sut.auth(mockAuthentication());
+        await expect(promise).rejects.toThrow(new InvalidCredentialsError());
     })
 })
