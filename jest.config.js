4f52741c3aff841a93d420ea284a83486e4cc4eb
Fabio <fabioac71@gmail.com>
2021-08-21 13:52:34 -0300

    chore: add react configs

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: ['src/**/*.ts(x)?'],
   modulePaths: ['<rootDir>/src/'],
   preset: 'ts-jest',
   transform: {
