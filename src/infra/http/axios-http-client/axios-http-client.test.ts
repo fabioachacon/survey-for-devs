aae315a2e9d015a6937c41df191d0b2043de0e8e
Fabio <fabioac71@gmail.com>
2021-12-06 17:01:33 -0300

    test: enruse that correct statusCode and body are returned on failure

--- src/infra/http/axios-http-client/axios-http-client.test.ts
+++ src/infra/http/axios-http-client/axios-http-client.test.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { mockAxios } from 'infra/test';
+import { mockAxios, mockHttpResponse } from 'infra/test';
 import { AxiosHttpClient } from './axios-http-client';
 import { mockPostRequest } from 'data/test';
 
@@ -30,6 +30,16 @@ describe('AxiosHttpClient', () => {
 
   test('Should return the correct StatusCode and body', () => {
     const { sut, mockedAxios } = makeSut();
+    const promise = sut.post(mockPostRequest());
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
+  });
+
+  test('Should return the correct statusCode and body on failure', () => {
+    const { sut, mockedAxios } = makeSut();
+    mockedAxios.post.mockRejectedValueOnce({
+      response: mockHttpResponse()
+    });
+
     const promise = sut.post(mockPostRequest());
     expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
   });
