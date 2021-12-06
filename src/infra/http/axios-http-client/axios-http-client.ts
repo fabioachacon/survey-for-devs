a6fd1bae3e9bef328fff8151fc47624b19902257
Fabio <fabioac71@gmail.com>
2021-12-06 17:11:52 -0300

    feat: adding error handling to AxiosHttpClient

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import {
   HttpPostClient,
   HttpPostParams,
@@ -7,7 +7,13 @@ import {
 
 export class AxiosHttpClient implements HttpPostClient {
   async post(params: HttpPostParams): Promise<HttpResponse> {
-    const httpResponse = await axios.post(params.url, params.body);
+    let httpResponse: AxiosResponse;
+    try {
+      httpResponse = await axios.post(params.url, params.body);
+    } catch (error) {
+      httpResponse = error.response;
+    }
+
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data
